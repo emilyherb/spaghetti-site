4febe4f63a6149417fd26b5ffb9fadfb8ee68b6f
Emily Herbert <120695456+emilyherb@users.noreply.github.com>
2024-10-10 03:28:40 +0000

    updates!

---
+++ main.js
@@ -0,0 +1,19 @@
+var textWrapper = document.querySelector('.header');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.header span',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.header',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
